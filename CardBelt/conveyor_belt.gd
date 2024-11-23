5f5560cb28af689a724400fc32ea24fbc97a8dad
xxxxxmm <alovroger@gmail.com>
2024-11-23 19:33:46 +0000

    use card through tapping

--- CardBelt/conveyor_belt.gd
+++ CardBelt/conveyor_belt.gd
@@ -1,12 +1,14 @@
 extends Node2D
-const baseSpeed = 50
+
+const baseSpeed = 20
 var beltSpeed = baseSpeed
-var acceleration = 10
+var acceleration = 15
 const deceleration = 30
-const maxSpeed = 200
+const maxSpeed = 100
 var isButtonHeld = false
 var tapThreshold = 0.2 
 var tapTimer = 0.0
+var detectedCard = null
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,18 +26,20 @@ func _process(delta: float) -> void:
 		tapTimer += delta
 		if(tapTimer > tapThreshold):
 			beltSpeed = min(beltSpeed + acceleration * delta, maxSpeed) 
-			update_card_velocity() 
+			
 			print(beltSpeed)
 		
 	if Input.is_action_just_released("BUTTON1"): 
 		isButtonHeld = false
 		if(tapTimer <= tapThreshold):
-			print("use item")
+			_use_card()
+			
 	
 	if not isButtonHeld and beltSpeed > baseSpeed: 
 		beltSpeed = max(beltSpeed - deceleration * delta, baseSpeed) 
-		update_card_velocity()
 		print(beltSpeed)
+	
+	_update_card_velocity() 
 
 
 func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
@@ -50,7 +54,20 @@ func _on_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, l
 		parent.updateOnBelt(false)
 
 
-func update_card_velocity() -> void: 
+func _update_card_velocity() -> void: 
 	var cards = get_tree().get_nodes_in_group("cards") 
 	for card in cards: 
 		card.updateVelocity(beltSpeed)
+
+
+#func _has_overlapping_areas() -> bool: 
+	#var overlapping_areas = $Belt/CursorArea.get_overlapping_areas() 
+	#return overlapping_areas.size() > 0
+
+func _use_card() -> void:
+	var overlapping_areas = $Belt/CursorArea.get_overlapping_areas()
+	for area in overlapping_areas: 
+		var parent = area.get_parent()
+		if parent.is_in_group("cards"): 
+			detectedCard = parent
+			print("use item", detectedCard)
