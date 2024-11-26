486c382c416557886de072e5fac3aaa1c0f50237
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2024-11-26 19:54:17 +0000

    conveyor_belt script changes
    I made it take one acceleration value.
    Internally it collects the speed values before making changes.

--- CardBelt/conveyor_belt.gd
+++ CardBelt/conveyor_belt.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 const baseSpeed = 20
+const baseAccel: float = 20
+const baseDeccel = 20
+
+var pressureMaxSpeedBonus: float = 0.0
+const churnMaxSpeedBonus: float = 180  
+
 var beltSpeed = baseSpeed
-var acceleration = 15
-const deceleration = 20
-const maxSpeed = 100
 
 var isButtonHeld = false
 var tapThreshold = 0.2 
@@ -17,42 +20,67 @@ var beltArea
 func _ready() -> void:
 	beltArea = $Belt/BeltArea.get_overlapping_areas()
 
-
+func set_pressure_bonus(val: float):
+	pressureMaxSpeedBonus = val
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("BUTTON1"):
 		tapTimer = 0.0
-		
-		
+	
 	if Input.is_action_pressed("BUTTON1"): 
 		isButtonHeld = true
 		tapTimer += delta
-		if(tapTimer > tapThreshold):
-			beltSpeed = min(beltSpeed + acceleration * delta, maxSpeed) 
-			
-			print(beltSpeed)
-		
+
 	if Input.is_action_just_released("BUTTON1"): 
 		isButtonHeld = false
+		tapTimer = 0.0
 		if(tapTimer <= tapThreshold):
 			_use_card()
-			
-	
-	if not isButtonHeld and beltSpeed > baseSpeed: 
-		beltSpeed = max(beltSpeed - deceleration * delta, baseSpeed) 
-		print(beltSpeed)
-	
-	_update_card_velocity() 
+
+	if !Input.is_action_pressed("BUTTON1"):
+		isButtonHeld = false
+		tapTimer = 0.0
+
+	# Churn bonus speed
+	var churn_bonus_max: float = 0.0
+	if(tapTimer > tapThreshold):
+		churn_bonus_max = churnMaxSpeedBonus
+
+	# Calculate overall acceleration and overall maximum speed
+	var maximum_speed: float = baseSpeed + churn_bonus_max + pressureMaxSpeedBonus
+	var accel: float = baseAccel * delta
+
+	# Mutually exclusive if statements below
+	if accel == 0.0 && beltSpeed == maximum_speed:
+		# Do nothing
+		_update_card_velocity() # exit  
+		return
+	if beltSpeed < maximum_speed: # Accelerate
+		var new_speed = beltSpeed + accel
+		if new_speed >= maximum_speed:
+			beltSpeed = maximum_speed 
+		else:
+			beltSpeed = new_speed
+		_update_card_velocity() # exit
+		return
+	if beltSpeed > maximum_speed: # Deccelerate
+		var new_speed = beltSpeed - baseDeccel
+		if new_speed <= maximum_speed:
+			beltSpeed = maximum_speed 
+		else:
+			beltSpeed = new_speed
+		_update_card_velocity() # exit
+		return
 
 
-func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+func _on_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
 	var parent = area.get_parent()
 	if parent.is_in_group("cards"):
 		parent.updateOnBelt(true)
 
 
-func _on_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+func _on_area_shape_exited(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
 	var parent = area.get_parent()
 	if parent.is_in_group("cards"):
 		parent.updateOnBelt(false)
