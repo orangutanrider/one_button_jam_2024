72ec3279e57184fa41d50fbd241bf37f471b9301
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2024-11-27 16:34:16 +0000

    conveyor_cursor

---
+++ CardBelt/scripts/conveyor_cursor.gd
@@ -0,0 +1,26 @@
+extends RayCast2D
+
+@export var params: ConveyorCursorRes
+
+var active_timer: float = 0.0
+
+func _physics_process(delta: float) -> void:
+    if active_timer <= 0.0:
+        return
+    active_timer = active_timer - delta
+
+    if !is_colliding():
+        return
+    
+    var detection: Object = get_collider()
+    if detection == null:
+        push_warning("Unexpected null detection")
+        return
+
+    #todo
+    pass
+
+## Activates the cursor to play a card underneath it
+func activate_cursor() -> void:
+    active_timer = params.active_time
+    pass
---
+++ CardBelt/scripts/conveyor_cursor_res.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name ConveyorCursorRes
+
+## This works like an input buffer, when it recieves the call to activate, it will be active for this long
+@export var active_time: float = 0.1
