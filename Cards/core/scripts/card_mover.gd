7233cf993564b92e9f484855d123f13a0318079a
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2024-11-27 23:56:05 +0000

    card jettison
    Jettison is what I call the state it is in while discarding but not yet discarded

--- Cards/core/scripts/card_mover.gd
+++ Cards/core/scripts/card_mover.gd
@@ -1,13 +1,28 @@
-# Handles the movement of the card
-
 extends RigidBody2D
+
+@export var params: CardMovementRes
 
 func ghost():
 	sleeping = true
 	freeze = true
+
+	lock_rotation = true
+	rotation = 0.0
+	gravity_scale = 0.0
 	pass
 
 func un_ghost():
 	sleeping = false
 	freeze = false
+
+	lock_rotation = true
+	rotation = 0.0
+	gravity_scale = 0.0
 	pass
+
+func jettison_trigger():
+	lock_rotation = false
+	angular_velocity = params.jettison_angular
+	gravity_scale = params.jettison_grav
+	pass
+
