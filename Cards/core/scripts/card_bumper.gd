e46d956f220b6921d43fdb34f6250f4b5aa23204
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2024-11-28 01:47:28 +0000

    sleep
    sleep

--- Cards/core/scripts/card_bumper.gd
+++ Cards/core/scripts/card_bumper.gd
@@ -1,4 +1,10 @@
+# When cards touch eachother they will bump eachother along by pushing the card further down the line forward
+
 extends Area2D
+
+# TODO
+# Make it push the other card beyond the edge of its own body
+# And then-some more based on the params
 
 func area_entered(area: Area2D):
 	pass
