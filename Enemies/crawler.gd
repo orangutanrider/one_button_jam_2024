756b3741c84f743e308ff5a7b1233481a522e173
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2024-12-01 21:09:59 +0000

    aggresive ants

--- Enemies/crawler.gd
+++ Enemies/crawler.gd
@@ -2,6 +2,7 @@ extends Enemy
 @export var speed = 5
 var speedMultiplier = 1000
 @export var kiteDistance = 1000
+@export var chase_distance = 10000
 @onready var spriteAnimator = $AnimatedSprite2D
 var stopToAttack : bool = false
 var stopTimer : float = 0
@@ -25,12 +26,23 @@ func _attack_extention() -> void:
 	stopToAttack = true
 
 func _kite_away(delta: float) -> void: 
-	if targetBody != null && !stopToAttack:
-		if abs(global_position.x - targetBody.global_position.x) < kiteDistance:
-			var direction = (global_position - targetBody.global_position).normalized()
-			direction.x = abs(direction.x)
-			velocity.x = direction.x * speed * delta * speedMultiplier
-			spriteAnimator.play("walk")
-		else:
-			velocity.x = 0
-			spriteAnimator.play("default")
+	if !(targetBody != null && !stopToAttack):
+		return
+
+	var dist = abs(global_position.x - targetBody.global_position.x)
+
+	if dist > kiteDistance && dist < chase_distance:
+		var direction = (global_position - targetBody.global_position).normalized()
+		direction.x = abs(direction.x)
+		velocity.x = -(direction.x * speed * delta * speedMultiplier)
+		spriteAnimator.play("walk")
+		return
+
+	if dist < kiteDistance:
+		var direction = (global_position - targetBody.global_position).normalized()
+		direction.x = abs(direction.x)
+		velocity.x = direction.x * speed * delta * speedMultiplier
+		spriteAnimator.play("walk")
+	else:
+		velocity.x = 0
+		spriteAnimator.play("default")
