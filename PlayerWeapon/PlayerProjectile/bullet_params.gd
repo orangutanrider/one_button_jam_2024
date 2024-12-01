d3335cdb4026351e8f702f71fceb8517092f0830
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2024-12-01 14:17:12 +0000

    ap shell updated

---
+++ PlayerWeapon/PlayerProjectile/bullet_params.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name BulletParams
+
+@export var speed: float = 1.0
+@export var damage: int = 1
--- PlayerWeapon/PlayerProjectile/custom/scripts/ap_shell_script.gd
+++ PlayerWeapon/PlayerProjectile/custom/scripts/ap_shell_script.gd
@@ -1,24 +1,28 @@
 extends MasterBullet
 
+@export var params: BulletParams
+
 var Fired : bool = 0
 
 func _on_bullet_fired() -> void:
 	Fired = 1
-	var velocity = Vector2.RIGHT
+	#var velocity = Vector2.RIGHT
 
-func _physics_process(delta: float) -> void:
-	const Speed = 300
+func _physics_process(_delta: float) -> void:
+	# const Speed = 300
 	if Fired:
-		velocity = Vector2(Speed,0).rotated(rotation)
+		velocity = Vector2(params.speed,0).rotated(rotation)
 		move_and_slide()
 
-func _on_area_2d_area_entered(body) -> void:
-	const Damage = 300
-	if Fired:
-		if body.collision_layer & (1<<0):
-			queue_free()
-		elif body.collision_layer & (1<<1):
-			print("Enemy Hit")
-			body.get_parent()._take_damage(Damage)
-			queue_free()
+func _on_area_2d_area_entered(body: Area2D) -> void:
+	#const Damage = 300
+	#if Fired:
+	#	if body.collision_layer & (1<<0):
+	#		queue_free()
+	#	elif body.collision_layer & (1<<1):
+	#		print("Enemy Hit")
+	#		body.get_parent()._take_damage(Damage)
+	#		queue_free()
+
+	body.get_parent()._take_damage(params.damage)
 		
