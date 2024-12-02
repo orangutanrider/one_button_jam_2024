168f711a0645cae9f0aa88d4ca952a980435c363
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2024-12-02 02:14:32 +0000

    adjustments

--- Tank/scripts/roller.gd
+++ Tank/scripts/roller.gd
@@ -2,5 +2,10 @@ extends Node2D
 
 @export var rotate_speed: float = 1.0
 
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	rotate(rng.randf_range(0, 6.28319))
+
 func _process(delta: float):
 	rotate(rotate_speed * delta)
